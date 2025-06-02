09450ac0a53c25c3aa8614fdc51b0f9b67a86cf4
ArjunaGunatamaSihombing <138564014+RjunKece@users.noreply.github.com>
2025-06-02 23:05:31 +0700

    Create logout.php

---
+++ PemWebProjectAkhir/proses/logout.php
@@ -0,0 +1,6 @@
+<?php
+session_start();
+session_unset(); // menghapus semua variabel session
+session_destroy(); // menghancurkan session
+header("Location: dashboard.php"); // balik ke halaman utama
+exit();

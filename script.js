5d2d8e3927ab37fc550c728fda40e0db42417be9
ArjunaGunatamaSihombing <138564014+RjunKece@users.noreply.github.com>
2025-06-02 23:01:24 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+localStorage.setItem("user", JSON.stringify({
+  username: "Arjuna",
+  loggedIn: true,
+  profilePicture: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
+}));

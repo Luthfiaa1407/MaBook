b10c3c64a13201e6f5a84d4a9005329f1d7673fe
luckyboy002 <151297077+luckyboy002@users.noreply.github.com>
2025-06-20 00:17:26 +0700

    Update footer.php

--- include/footer.php
+++ include/footer.php
@@ -8,7 +8,7 @@
             <a href="#">FAQ</a>
             <a href="#">Panduan Penggunaan</a>
             <a href="#">Kontak Kami</a>
-            <a href="#">Laporan Masalah</a>
+            <a href="/mabook/user/laporan-create.php">Laporan Masalah</a>
         </div>
         <div class="flex flex-col gap-4 items-start justify-start">
             <div class="flex flex-col gap-3">
